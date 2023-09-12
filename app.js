6fb1817c4210bcc41690d486cc0eda7ba46f54b8
HaseebNaeemUddin <Sheikh.haseeb001@gmail.com>
2023-09-12 11:51:53 +0500

    add files

---
+++ app.js
@@ -0,0 +1,9 @@
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>Chap9-11>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// Q1
+
+var userInput = propmt("Enter your city");
+if (userInput === karachi){
+    console.log("Welocme to the city of lights");
+}
+
